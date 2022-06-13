c06275ea3364c5e363bc84621e1a4470fb8859fd
R-S-GREWAL <88488978+R-S-GREWAL@users.noreply.github.com>
2022-06-13 18:02:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,9 +22,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
       console.log(results);
-      console.log("nose x = " + results[0].pose.nose.x);
-      console.log("nose y = " + results[0].pose.nose.y);
-    }
+      noseX= results[0].pose.nose.x;
+      noseY= results[0].pose.nose.y;
+      console.log("nose x = " + noseX);
+      console.log("nose y = " + noseY);    }
 }
 function draw() {
 image(video, 0, 0, 300, 300);
